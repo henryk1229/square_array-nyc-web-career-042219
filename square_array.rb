625d331a3b319909e29ba64eaa8d3a90f49982b2
Henry Koehler <henryk1229@gmail.com>
2019-04-02 20:30:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(numbers)
   new_array = []
   numbers.each do | numbers |
-    new_array.push("#{numbers[i]}**")
+    new_array.push("#{numbers{i}**")
+    numbers += 1
   end
   new_array
 end
